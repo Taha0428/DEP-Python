4515284b2a2528d5ffdee5bf435db89258996bdc
Taha0428 <tahaj0336@gmail.com>
2024-09-16 21:25:37 +0500

    Update Task 2.py

--- Task 2/Task 2.py
+++ Task 2/Task 2.py
@@ -5,13 +5,12 @@ import csv
 url = 'http://quotes.toscrape.com/'
 page = 1
 
-# Open CSV file to write the data
 with open('quotes_data.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow(["Quote", "Author", "Tags"])
 
     while True:
-        # Request the page
+    
         try:
             response = requests.get(url + f'page/{page}/')
             response.raise_for_status()
@@ -21,13 +20,11 @@ with open('quotes_data.csv', 'w', newline='', encoding='utf-8') as file:
 
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Get all quotes on the page
         quotes = soup.find_all('div', class_='quote')
         if not quotes:
             print(f"No more quotes found. Scraping completed at page {page}.")
             break
 
-        # Extract and write quote data to CSV
         for quote in quotes:
             text = quote.find('span', class_='text').text
             author = quote.find('small', class_='author').text
